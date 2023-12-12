4203d1e4b56100f75041651222900e06885d7a0d
Sujit Sable <sujitsable92@gmail.com>
2023-12-13 00:43:17 +0530

    Done with the basic http server

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/devil1229/Chirpy
 
 go 1.20
+
+require github.com/go-chi/chi v1.5.5
--- main.go
+++ main.go
@@ -1,24 +1,36 @@
 package main
 
 import (
-	//"fmt"
+	"fmt"
 	"log"
 	"net/http"
 )
 
+type apiConfig struct {
+	fileserverHits int
+}
+
+
 func main() {
 	mux := http.NewServeMux()
-	corsMux := middlewareCors(mux)
+	logmux := middlewareLog(mux)
+	corsMux := middlewareCors(logmux)
+	apiConfig := apiConfig{
+		fileserverHits: 0,
+	}
+	
 
 	server := &http.Server{
 		Addr:    ":8080",
 		Handler: corsMux,
 	}
 
-	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	mux.Handle("/app/", http.StripPrefix("/app", apiConfig.middlewareMetricsInc(http.FileServer(http.Dir(".")))))
     //mux.Handle("/assets", http.FileServer(http.Dir(".")))
 
 	mux.HandleFunc("/healthz", serverHealth)
+	mux.HandleFunc("/metrics", apiConfig.totalHits)
+	mux.HandleFunc("/reset", apiConfig.resetHits)
     log.Printf("Serving files from webserver on port: %v\n", 8080)
 	if err := server.ListenAndServe(); err != nil {
 		panic(err)
@@ -44,3 +56,36 @@ func serverHealth(w http.ResponseWriter, r *http.Request){
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("OK"))
 }
+
+func (cfg *apiConfig) totalHits(w http.ResponseWriter, r *http.Request){
+	x := cfg.fileserverHits
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	responseString := fmt.Sprintf("Hits: %d", x)
+	w.Write([]byte(responseString))
+}
+
+func (cfg *apiConfig) resetHits(w http.ResponseWriter, r *http.Request){
+	cfg.fileserverHits = 0
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	//responseString := fmt.Sprint("Reset Successful")
+	w.Write([]byte("Reset Successful"))
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {	
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits = cfg.fileserverHits + 1
+	    log.Printf("Hits: %v", cfg.fileserverHits)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func middlewareLog(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("%s %s", r.Method, r.URL.Path)
+		next.ServeHTTP(w, r)
+	})
+}
+
+
